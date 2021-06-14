7b1399ecb4ead625dcb356d445d8ef96b64ddcd7
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-06-13 21:59:20 -0400

    add programmatic import of modules (for testing)

--- imitate.py
+++ imitate.py
@@ -2,3 +2,30 @@ import numpy as np
 from matplotlib.image import imread
 import importlib.util
 import sys
+
+# from ..textdiagrams.geometry import *
+sys.path.insert(0, '../text-diagrams/geometry')
+
+class Geometry:
+    pass
+
+modules = ['point', 'line', 'polygon']
+mlist = []
+for m in modules:
+    # path = f'../text-diagrams/geometry/{m}.py'
+    # print(path)
+    # spec = importlib.util.spec_from_file_location(m, path)
+    # f = importlib.util.module_from_spec(spec)
+    # spec.loader.exec_module(f)
+
+    f = importlib.import_module(m)
+    setattr(Geometry, m, f)
+    mlist.append(f)
+
+# for m in mlist:
+#     spec.loader.exec_module(m)
+
+G = Geometry
+
+p = G.point.Point([1, 1])
+p.print()
