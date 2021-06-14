3a3946b8f147e8e83b28878c94fc03928167726b
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-06-13 22:43:52 -0400

    add evolutionary search

--- imitate.py
+++ imitate.py
@@ -6,6 +6,7 @@ import glob
 from PIL import Image, ImageDraw
 import string
 import random
+import copy
 
 # from ..textdiagrams.geometry import *
 sys.path.insert(0, '../text-diagrams/geometry')
@@ -41,24 +42,43 @@ population = []
 
 # generated = np.zeros_like(image_data)
 height, width, channels = image_data.shape
+num = 5
 
-for i in range(20):
+for i in range(num):
     group = [[], None]
     dims = (width, height)
     generated = Image.new('RGB', dims, (255,)*3)
     d = ImageDraw.Draw(generated)
-    for j in range(500):
+    for j in range(100):
         a = [None] * 2
         pos = np.random.randint([0, 0], dims, [2])
         # print(pos)
         text = random.choice(string.ascii_letters)
         color = [255, 0, 0]
         d.text(tuple(pos), text, fill=tuple(color))
-        a = pos, text
+        a = [pos, text, color]
         group[0].append(a)
     difference = np.abs(generated - image_data).mean()
     group[1] = difference
     population.append(group)
+
+for i in range(5):
+    population.sort(key=lambda x: x[1])
+    # print(population)
+    best = population[0]
+    print(best[1])
+    population = [copy.deepcopy(best) for n in range(num)]
+    for j, g in enumerate(population):
+        generated = Image.new('RGB', dims, (255,)*3)
+        d = ImageDraw.Draw(generated)
+        for l in g[0]:
+            # print(l)
+            l[0] += np.random.normal(-10, 10, [2]).astype(int)
+            pos, text, color = l
+            d.text(tuple(pos), text, fill=tuple(color))
+
+        difference = np.abs(generated - image_data).mean()
+        population[j][1] = difference
 
 # generated.show()
 generated.save('./result-image.png')
